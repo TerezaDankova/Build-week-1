9158acfd2c193b6282df910a6c079d29127b463b
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-05-18 10:57:56 +0200

    adding a button html/ adding js

---
+++ index.js
@@ -0,0 +1,15 @@
+const checkbox =document.querySelector("#checkbox")
+checkbox.addEventListener("click", function () {
+  checkbox.classList.toggle("selected")
+ 
+
+
+})
+
+
+function proceedButton () {
+  if(checkbox.classList.contains("selected")) { 
+     window.location.href = "index2.html"; } 
+}
+
+
